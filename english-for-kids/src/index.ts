c8b6826a1dd8c5facefeaf528bea2dc84bb6cb3c
elenaniksy <elfhelen@gmail.com>
2021-06-23 17:00:33 +0300

    feat: add styles.scss, main background to the application

---
+++ english-for-kids/src/index.ts
@@ -0,0 +1,7 @@
+import './style.scss';
+
+window.onload = () => {
+  const appElement = document.getElementById('app');
+  if (!appElement) throw Error('Application root element not found');
+  // const application = new App(document.body);
+};
