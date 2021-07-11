40668aa334215ef21f6511acc6dad83cdc361fbb
elenaniksy <elfhelen@gmail.com>
2021-07-11 15:12:14 +0300

    fix: fix syntax

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,19 @@
-import path from 'path';
-import express from 'express';
+import express, { Application, Request, Response} from 'express';
+const path = require ('path');
 import bodyParser from "body-parser";
 import cors from 'cors';
 const PORT = process.env.PORT || 80;
+const app: Application = express();
 
-const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 const publicPath = path.resolve(__dirname, '../english-for-kids/dist');
-console.log(publicPath);
 const indexPath = path.resolve(__dirname, '../english-for-kids/dist/index.html');
 
 app.use(/^(?!\/api\/)/, express.static(publicPath));
-app.use(/^(?!\/api\/)/, (req, res) => {
+app.use(/^(?!\/api\/)/, (req: Request, res: Response) => {
   res.sendFile(indexPath);
 });
 
