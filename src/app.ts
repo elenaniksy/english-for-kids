e9f44e8b8dbf3772d92ef76d6cf3303c85a366d8
elenaniksy <elfhelen@gmail.com>
2021-07-11 16:26:08 +0300

    fix:fix syntax

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response} from 'express';
-const path = require ('path');
-import bodyParser from "body-parser";
+import express, { Application, Request, Response } from 'express';
+import path from 'path';
+// const path = require('path');
+import bodyParser from 'body-parser';
 import cors from 'cors';
 const PORT = process.env.PORT || 80;
 const app: Application = express();
@@ -10,12 +11,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const publicPath = path.resolve(__dirname, '../english-for-kids/dist');
-const indexPath = path.resolve(__dirname, '../english-for-kids/dist/index.html');
+const indexPath = path.resolve(
+  __dirname,
+  '../english-for-kids/dist/index.html'
+);
 
 app.use(/^(?!\/api\/)/, express.static(publicPath));
 app.use(/^(?!\/api\/)/, (req: Request, res: Response) => {
   res.sendFile(indexPath);
 });
 
-app.listen(PORT, ()=> console.log('Server started on http://localhost:80'))
-
+app.listen(PORT, () => console.log('Server started on http://localhost:80'));
